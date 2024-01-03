e5f183fa9e415ea1bedd2781432f5923a95ddf44
user <hongsch07@naver.com>
2024-01-03 16:48:20 +0900

    C++ Quickstudy - input

---
+++ cpp/cpp-quickstudy/cc-input.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void) {
+    int in1;
+    std::cin >> in1;
+
+    int in2;
+    std::cin >> in2;
+
+    int result = in1 + in2;
+    std::cout << in1 << "+" << in2 << "=" << result << std::endl;
+
+    return 0;
+}
